8cdc8ca1f3f29b23e5e5461157bfdfa12c5bdd69
Wade Edwards <wadecedwards@gmail.com>
2022-06-20 13:31:15 -0500

    Add files via upload

--- main.js
+++ main.js
@@ -27,31 +27,17 @@ fadeIn.forEach(element => {
 });
 
 
-// Mobile menu
-const menuBtn = document.querySelector('.menu');
-const closeBtn = document.querySelector('.close');
-const menu = document.querySelector('.mobile-menu-page');
+const tabs = document.querySelectorAll('.tab');
+const homeTab = document.querySelector('.home-tab');
 
-menuBtn.addEventListener('click', () => {
-  menuBtn.style.visibility = 'hidden';
-  menuBtn.style.transform = 'translateY(20px)';
-  menuBtn.style.transition = 'transform 600ms 300ms ease, visibility 600ms ease';
-  closeBtn.style.visibility = 'visible';
-  closeBtn.style.transform = 'translateY(0)';
-  closeBtn.style.transition = 'transform 600ms 900ms ease, visibility 800ms 1s ease';
-  menu.style.opacity = '1';
-  menu.style.visibility = 'visible';
-  menu.style.transform = 'translateX(0)';
+document.addEventListener('DOMContentLoaded', () => {
+  homeTab.classList.add('active-tab');
 });
 
-closeBtn.addEventListener('click', () => {
-  menuBtn.style.visibility = 'visible';
-  menuBtn.style.transform = 'translateY(0)';
-  menuBtn.style.transition = 'transform 600ms 900ms ease, visibility 600ms 1s ease';
-  closeBtn.style.visibility = 'hidden';
-  closeBtn.style.transform = 'translateY(-20px)';
-  closeBtn.style.transition = 'transform 600ms 300ms ease, visibility 600ms ease';
-  menu.style.opacity = '0';
-  menu.style.visibility = 'hidden';
-  menu.style.transform = 'translateX(500px)';
-})
+
+tabs.forEach(tab => {
+  tab.addEventListener('click', () => {
+    document.querySelector('.tab.active-tab').classList.remove('active-tab');
+    tab.classList.toggle('active-tab');
+  });
+});
